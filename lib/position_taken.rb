f4638d15aa9df0cdd5829759d936d292601d514c
Sheila Wittman <szandora789@gmail.com>
2018-05-10 00:19:08 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,11 +1,9 @@
 # code your #position_taken? method here!
 
-board = ["",""," "," ","X"," "," "," ",""]
+#board = ["",""," "," "," "," "," "," ",""]
 
-def position_taken?(board, index)
-! (board[index] ==  " " || board[index] == "" || board[index] == nil) ?
-    false : true
-
-  #This was mine:
-  ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
+#def position_taken?(board, index)
+#!(board[index] == " " || board[index] == "" || board[index] == nil?)?
+#false: true
+ ! (board[index] ==  " " || board[index] == "" || board[index] == nil)
 end
